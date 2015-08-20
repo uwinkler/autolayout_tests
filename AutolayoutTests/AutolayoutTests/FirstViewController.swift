0db99e9fc4501811ccb54945975fffd8142f6e0f
uwinkler <uwinkler@kodira.de>
2015-08-20 11:45:17 +0200

    Initial Commit

---
+++ AutolayoutTests/AutolayoutTests/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AutolayoutTests
+//
+//  Created by Ulrich Winkler on 20/08/15.
+//  Copyright (c) 2015 Kodira. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AutolayoutTests/AutolayoutTests/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AutolayoutTests
+//
+//  Created by Ulrich Winkler on 20/08/15.
+//  Copyright (c) 2015 Kodira. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
